39dcefc84671a38de0e38d06fc4e7477cd41a59a
li932565422 <35482711+li932565422@users.noreply.github.com>
2018-01-20 10:08:26 +0800

    qq

--- newAR/build/aframe-ar.js
+++ newAR/build/aframe-ar.js
@@ -1185,8 +1185,8 @@ var Qb=[Ik,Zh,_h,Qj,Qi,Pi,Ri,Ag,sg,qg,rg,yg,kh,jh,Oi,Mj];var Rb=[Jk,ki,ji,gi];va
 						facingDir = facing.exact;
 					}
 					for (var i=0; i<sources.length; i++) {
-						if (sources[i].kind === 'video' && sources[i].facing === facingDir) {
-							hdConstraints.video.mandatory.sourceId = sources[i].id;
+						if (sources[0].kind === 'video' && sources[0].facing === facingDir) {
+							hdConstraints.video.mandatory.sourceId = sources[0].id;
 							break;
 						}
 					}
