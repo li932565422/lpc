8671125aa640e51fa55c6e768eceb8d51f7f8317
li932565422 <35482711+li932565422@users.noreply.github.com>
2018-07-06 11:24:58 +0800

    Update Main.java

--- SimpleThreeJsExample/backend/Java/src/Main.java
+++ SimpleThreeJsExample/backend/Java/src/Main.java
@@ -6,9 +6,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        String cloudKey = "<这里是云图库的Cloud Key>";
-        String cloudSecret = "<这里是云图库的Cloud Secret>";
-        String cloudUrl = "http://<这里是云图库的Client-end URL>/search";
+        String cloudKey = "65dce09a71c50c638e0cb3f386da8396";
+        String cloudSecret = "hc5fF3rXRfAFJeAThKA1WQZ1PYIhZrl02A7YeQ6cB7x3hX4PEiO4B8mO2nzYWQntWPSltTJG5gL7y13QMAx3C6RJCtXFxsgm9kigQKJq6I7WkNYosOhYzVg1bohVqz8Y";
+        String cloudUrl = "http://b21934dca51e397a6b2f10702ef84820.cn1.crs.easyar.com:8080/search";
 
         WebAR webAR = new WebAR(cloudKey, cloudSecret, cloudUrl);
 
