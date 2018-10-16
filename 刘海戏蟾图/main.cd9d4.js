1adac34a871a65b9be673523e21c7287fc1146a7
li932565422 <35482711+li932565422@users.noreply.github.com>
2018-10-16 16:21:57 +0800

    正式版1.0

--- 刘海戏蟾图/main.cd9d4.js
+++ 刘海戏蟾图/main.cd9d4.js
@@ -155,7 +155,7 @@
         var jsList = settings.jsList;
 
         if (!false) {
-            var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.cc8e5.js';
+            var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.44d68.js';
             if (jsList) {
                 jsList = jsList.map(function (x) {
                     return 'src/' + x;
@@ -212,7 +212,7 @@
     }
 
     if (window.jsb) {
-        require('src/settings.31133.js');
+        require('src/settings.2c62a.js');
         require('src/jsb_polyfill.js');
         boot();
         return;
