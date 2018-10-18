5776b3ee5e5b2e0758b29f7e39173f166cbec713
li932565422 <35482711+li932565422@users.noreply.github.com>
2018-10-18 11:11:37 +0800

    旋转bug

--- 刘海戏蟾图/main.e92ec.js
+++ 刘海戏蟾图/main.e92ec.js
@@ -155,7 +155,7 @@
         var jsList = settings.jsList;
 
         if (!false) {
-            var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.691c2.js';
+            var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.81af4.js';
             if (jsList) {
                 jsList = jsList.map(function (x) {
                     return 'src/' + x;
