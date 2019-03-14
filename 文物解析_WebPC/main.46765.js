d5bc87aecfda96a34cf4ae440d7d5c56815ddd4c
li932565422 <35482711+li932565422@users.noreply.github.com>
2019-03-14 17:14:43 +0800

    文物解析2.0

--- 文物解析_WebPC/main.46765.js
+++ 文物解析_WebPC/main.46765.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.0100a.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.31d89.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -161,7 +161,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.98b2b.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.46765.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -185,7 +185,7 @@ if (false) {
     window.boot();
 }
 else if (window.jsb) {
-    require('src/settings.60090.js');
+    require('src/settings.d1fbc.js');
     require('src/cocos2d-jsb.js');
     require('jsb-adapter/engine/index.js');
     window.boot();
